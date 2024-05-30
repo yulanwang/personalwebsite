e6ca00cc7f3d5c42a4811def9818327114f6b773
yulanwang <yulanw16@gmail.com>
2024-05-29 21:06:50 -0700

    change img size

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe () {
     return (
         <section id="AboutMe" className="about--section">
             <div className="about--section--img">
-                <img src="./img/Wang,Yulan copy.jpg" alt="About Me"/>
+                <img src="./img/Wang,Yulan copy.jpg" alt="About Me" width="300" height="400"/>
             </div>
             <div className="hero--section--content--box about--section--box">
                 <div className="hero--section--content">
