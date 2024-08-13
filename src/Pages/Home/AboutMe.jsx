295607ab27d8d2bb8d7fc77a0d76eddb0bb224cf
yulanwang <yulanw16@gmail.com>
2024-08-13 12:06:53 -0400

    update about me

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -8,7 +8,7 @@ export default function AboutMe () {
                 <div className="hero--section--content">
                     <p className="section--title">About</p>
                     <h1 className="skills-section--heading">About Me</h1>
-                    <p className="hero--section-description">I study Computer Science with a minor in Mathematics at Northeastern University in Boston, MA.</p>
+                    <p className="hero--section-description">I study Computer Science and Mathematics at Northeastern University in Boston, MA.</p>
                     <p className="hero--section-description">I'm passionate about building inspiring websites, visualizing data for good, and applying natural language processing on social network data.</p>
                 </div>
             </div>
